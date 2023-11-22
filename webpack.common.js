cc5878b4040d63b62c57d97bc9c89e1416b622ee
Difana Nanda Pridhasila Zein <96951083+nandaazein@users.noreply.github.com>
2023-11-23 06:37:49 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -95,4 +95,11 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin(),
   ],
+});
+process.env.NODE_ENV === 'production' && new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+  ].filter(Boolean),
+};
 };
